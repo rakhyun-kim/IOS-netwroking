c38e74f9e60ac3081cf340ff1ae9db8fac74c399
RAKHYUN <kyle2598@gmail.com>
2023-10-02 10:06:40 -0500

    Initial Commit

---
+++ IOS netwroking/IOS_netwrokingApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_netwrokingApp.swift
+//  IOS netwroking
+//
+//  Created by Rakhyun Kim on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct IOS_netwrokingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
